e64756772ae9846ae727f7f714651a5fe107b2fb
nou-ali <noura4403@gmail.com>
2021-12-01 20:04:58 +0000

    name appeared on game

--- client.js
+++ client.js
@@ -1,25 +1,7 @@
 const net = require("net");
+const {connect} = require('./play')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: "50541"
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("recived data connection is establisted")
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
-
-
 
 
