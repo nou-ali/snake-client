e640ab2dac7bf8728add0e56470b9614884fe696
nou-ali <noura4403@gmail.com>
2021-12-02 02:39:10 +0000

    worked on constant values

--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MESSAGES} = require("./constants");
 const net = require("net");
 
 let connection;
@@ -8,31 +9,33 @@ const setupInput = function (conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput); 
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
 
-const handleUserInput = function(key) {
+const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up")
-    connection.write("Say: Hii")
+    //connection.write("Say: Hii")
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left")
-    connection.write("Say: it's been a long day")
+    //connection.write("Say: it's been a long day")
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down")
-    connection.write("Say: hope you'll get some rest")
+    //connection.write("Say: hope you'll get some rest")
   }
 
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right")
-    connection.write("Peace out")
+    //connection.write("Peace out")
+  } if (MESSAGES[key]) {
+    connection.write(MESSAGES[key])
   }
 };
 
