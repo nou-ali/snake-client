d132d6c29cb06e3bf32c10998e10801146cead62
nou-ali <noura4403@gmail.com>
2021-12-02 02:41:11 +0000

    worked on constant values

---
+++ constants.js
@@ -0,0 +1,27 @@
+IP = "165.227.47.243";
+PORT =  "50541";
+
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+const MESSAGES = {
+  x: "Say: Hii",
+  y:"Say: it's been a long day",
+  y: "Say: hope you'll get some rest",
+};
+
+
+
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES
+};
