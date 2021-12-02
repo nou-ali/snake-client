5e3b6c3e04c5caf66eda630605abd29534da4a46
nou-ali <noura4403@gmail.com>
2021-12-02 01:39:01 +0000

    added messages to server

--- input.js
+++ input.js
@@ -19,18 +19,21 @@ const handleUserInput = function(key) {
   }
   if (key === 'w') {
     connection.write("Move: up")
+    connection.write("Say: Hii")
   }
   if (key === 'a') {
     connection.write("Move: left")
+    connection.write("Say: it's been a long day")
   }
   if (key === 's') {
     connection.write("Move: down")
+    connection.write("Say: hope you'll get some rest")
   }
 
   if (key === 'd') {
     connection.write("Move: right")
+    connection.write("Peace out")
   }
-
 };
 
 
