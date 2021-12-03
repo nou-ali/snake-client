d36199ee8b4008a9f857f3fbb6d82928affcd250
nou-ali <noura4403@gmail.com>
2021-12-03 17:51:06 +0000

    client.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
-const {connect} = require('./play')
-const {setupInput} = require("./input")
+const {connect} = require('./play');
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 //connect();
